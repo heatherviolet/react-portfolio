8c826d0f4ef5447e12cafe0896135a0124438e9c
Heather Violet <heatherviolet91@gmail.com>
2022-04-10 18:38:35 -0700

    Updates

---
+++ precache-manifest.5fb2d7678f1a7361c8b62c4c45fb251a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d5a78d82ecaa99a6e1c",
+    "url": "/react-portfolio/static/css/main.24beb2f7.chunk.css"
+  },
+  {
+    "revision": "6d5a78d82ecaa99a6e1c",
+    "url": "/react-portfolio/static/js/main.8f3d7338.chunk.js"
+  },
+  {
+    "revision": "1061d8b9d78acc3ccf32",
+    "url": "/react-portfolio/static/js/runtime~main.5c63a2e3.js"
+  },
+  {
+    "revision": "f2e363b64ddd282d7d3e",
+    "url": "/react-portfolio/static/css/2.85537e14.chunk.css"
+  },
+  {
+    "revision": "f2e363b64ddd282d7d3e",
+    "url": "/react-portfolio/static/js/2.2fa0e493.chunk.js"
+  },
+  {
+    "revision": "9c4f68d8aa05f030962b",
+    "url": "/react-portfolio/static/js/3.4f5a5223.chunk.js"
+  },
+  {
+    "revision": "d30483e7063c65345d9b2cb1545c73cc",
+    "url": "/react-portfolio/static/media/self.d30483e7.jpg"
+  },
+  {
+    "revision": "dad2ae7afabaa1184692332a46d46765",
+    "url": "/react-portfolio/static/media/space.dad2ae7a.jpg"
+  },
+  {
+    "revision": "c4622f09bc468da8cf841776c00addc0",
+    "url": "/react-portfolio/static/media/allergy-free.c4622f09.jpg"
+  },
+  {
+    "revision": "c64a41b97f2cd59cc8e1718f6ea346da",
+    "url": "/react-portfolio/static/media/e-commerce.c64a41b9.jpg"
+  },
+  {
+    "revision": "b5fb591302f81e53d1fd8784ace5192d",
+    "url": "/react-portfolio/static/media/imdb.b5fb5913.jpg"
+  },
+  {
+    "revision": "f7f009660b1e9639f56e882121896a7c",
+    "url": "/react-portfolio/static/media/readme.f7f00966.jpg"
+  },
+  {
+    "revision": "e0fe912fc6a37bd42f0007fa7d681f0e",
+    "url": "/react-portfolio/static/media/portfolio.e0fe912f.jpg"
+  },
+  {
+    "revision": "2b2175e31119676a35be42f8225db545",
+    "url": "/react-portfolio/static/media/screen.2b2175e3.jpg"
+  },
+  {
+    "revision": "bb9a333f354958e58448a4267719fac9",
+    "url": "/react-portfolio/index.html"
+  }
+];
